0db38c16d154153141cf80b5242896aee7162973
rockyFierro <finnslighthouse@gmail.com>
2020-08-16 19:39:56 -0700

    Took loinger than expected to remember Routing... heh...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import {Route, Switch} from "react-router-dom";
+// import Navigation from "./Components/Navigation";
+import Home from "./Components/Home";
+import Header from "./Components/Header";
+import Form from "./Components/Form";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+  	<>
+  	<Header />
+  	<div className="reactBody">
+  		<Switch>
+  			<Route path="/Pizza">
+  				<Form />
+  			</Route>
+  			<Route path="/">
+  				<Home/>
+  			</Route>
+  		</Switch>
+  	</div>
+  	</>
   );
 };
 export default App;
---
+++ src/Components/Form.js
@@ -0,0 +1,14 @@
+//form
+import React from "react";
+
+const Pizza =()=>{
+	return(
+		<>
+			<div>
+				<h2> Pizza Order Form </h2>
+			</div>
+		</>
+		);
+}
+
+export default Pizza;
