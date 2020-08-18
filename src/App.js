6ac912cc771285064ab9c07952e69f5fa9565a7a
rockyFierro <finnslighthouse@gmail.com>
2020-08-17 18:24:27 -0700

    zero hour duex

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React from "react";
-import {Route, Switch} from "react-router-dom";
-// import Navigation from "./Components/Navigation";
-import Home from "./Components/Home";
-import Header from "./Components/Header";
-import Form from "./Components/Form";
+import {Switch, Route, Link} from "react-router-dom";
+import Form from "./Components/order.jsx";
 
-const App = () => {
 
-	
-  return (
-  	<>
-  	<Header />
-  	<div className="reactBody">
-  		<Switch>
-  			<Route path="/Pizza">
-  				<Form />
-  			</Route>
-  			<Route path="/">
-  				<Home/>
-  			</Route>
-  		</Switch>
-  	</div>
-  	</>
-  );
+export default function App(){
+  
+  const Home = () => {
+    return (
+        <div className="homeWrapper">
+          <h1>Lambda Eats</h1>
+          <p>Pizza style pizza arcade.</p>
+        </div>
+      );
+  };
+
+
+  return(
+    <div className="bod">
+      <nav>
+          <Link to="/pizza"><button>Order Form</button></Link>
+          <Link to="/" exact>Home</Link>
+      </nav>
+
+      <div className="pizzabox">
+        <Switch>
+          <Route path="/pizza"><Form/></Route>
+          <Route path="/" exact><Home/></Route>
+        </Switch>
+      </div>
+      
+    </div>
+    );
 };
-export default App;
